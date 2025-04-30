7def94f555d4f470ff8e16254c61376712e4f944
Apple <mac@Apples-MacBook-Pro.local>
2025-04-30 09:59:17 +0300

    I changed the ProjectsPage discreption for second time.

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -8,10 +8,7 @@ export default function ProjectsPage() {
       <h1> ⃣ Projects</h1>
       <div className="about-projects">
         <p>
-          I'm a paragraph. Click here to add your own text and edit me. It’s
-          easy. Just click “Edit Text” or double click me to add your own
-          content and make changes to the font. I’m a great place for you to
-          tell a story and let your users know a little more about you.
+          I'm a full-stack web developer open to work with any kind of sofware based project I am looking forward to work with you.
         </p>
       </div>
       <ProjectCard
